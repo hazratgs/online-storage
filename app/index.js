7b69ab6cdcc2d5382e4dbdcc77bb101d4121371b
hazratgs <hazratgs@gmail.com>
2018-02-02 13:10:27 +0300

    Добавление данных в базу

--- app/index.js
+++ app/index.js
@@ -3,10 +3,11 @@ const guid = require('uuid/v1')
 
 // Models
 const TokenModel = require('./models/token');
+const StorageModel = require('./models/storage')
 
 module.exports = app => {
 	// Создание токена
-	app.get('/create', (req, res) => {
+	app.post('/create', (req, res) => {
 		try {
 			// Новый уникальный uuid токен
 			const token = guid()
@@ -19,6 +20,33 @@ module.exports = app => {
 
 		} catch (e) {
 			res.status(500).send({ status: false, description: 'Ошибка при создании токена' })
+		}
+	})
+
+	// Добавление данных
+	app.post('/set/:token', async (req, res) => {
+		try {
+			const token = req.params.token
+
+			// Проверка токена 
+			const findToken = await TokenModel.Token.findOne({ token: token })
+			if (!findToken) throw new Error()
+
+			// Данные в storage
+			const storage = await StorageModel.Storage.findOne({ token: token })
+
+			if (storage) {
+				// Мердж новых и старых данных (перезапись)
+				const data = { ...storage.storage, ...req.body }
+				// Обновление данных
+				await StorageModel.Storage.update({ token: token }, { $set: { storage: data } })
+			} else {
+				await new StorageModel.Storage({ token: token, storage: req.body }).save()
+			}
+
+			res.json({ status: true, message: 'Успешно добавлено' })
+		} catch (e) {
+			res.status(404).send({ status: false, description: 'Ошибка добавления данных' })
 		}
 	})
 
